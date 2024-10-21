15f9fb4a82a7f82b58df53f79eb4cfd2c858120e
Katya <gloooomrine@gmail.com>
2024-10-21 18:05:55 +0300

    final version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let result = '';
+
+    if (n < 0)
+        str = str.slice(1);
+
+    for (let i = 0; i < str.length; i++){
+        result += str.at(-1);
+        str = str.slice(0, -1);
+    }
+    result += str.at(-1);
+
+    return (Number(result));
 }
